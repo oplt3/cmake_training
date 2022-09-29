7eb17c58ab23021fa5a57be48ae6358fbba2f595
Olivia Palut <olivia.palut@surgiqual-institute.com>
2022-09-29 17:50:24 +0200

    Link library to an executable

--- cmake_training/app/Main.cpp
+++ cmake_training/app/Main.cpp
@@ -2,9 +2,12 @@
 //! \brief Implements CMain
 
 #include <iostream>
+#include <animals/Dog.h>
 
 int main()
 {
-    std::cout << "Hello World ! " << std::endl;
+    std::cout << "Hello World!" << std::endl;
+    animals::CDog l_MyDog("Milou");
+    std::cout << "My dog's name is " << l_MyDog.GetName() << " !" << std::endl;
     return 0;
 }
